71a2bc0c16432333f9fb199d14fce9808a0c4cf5
DhokeyNikita <nikitadhokey94@gmail.com>
2018-12-05 12:11:25 +0530

    By Nikita

---
+++ MavenDemo/src/main/java/NewProject/DemoFirst.java
@@ -0,0 +1,26 @@
+package NewProject;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoFirst {
+
+	public WebDriver driver;
+	Logger log = Logger.getLogger("TestChrome");
+	public void openBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Nikhil\\git\\MavenDemo\\Drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		log.info("Opening browser...");
+		driver.manage().window().maximize();	
+	}
+
+	public void closeBrowser()
+	{
+		driver.quit();
+		log.info("Closing the browser...");
+	}
+}
